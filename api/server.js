dfc9fa1f952b9a1e8df42e0656b78702a48d3bde
wolfmaster8 <jflobom@gmail.com>
2018-11-06 19:22:38 -0200

    Added connection with DB 🗳️

--- api/server.js
+++ api/server.js
@@ -3,4 +3,33 @@ const mysql = require('mysql');
 
 const app = express();
 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  port: 8889,
+  database: 'easylearn'
+});
+
+conn.connect(function(e){
+  if(e){
+    console.log('Error Connecting');
+  }else{
+    console.log('Connected')
+  }
+});
+
+app.get('/usuarios', function(req, res){
+  conn.query("SELECT * FROM usuarios", function(error, rows, fields){
+    if(!!error) {
+      console.log('Error');
+    }else{
+      console.log('Success\n');
+      console.log(rows);
+      res.send('Hola '+rows[1].name)
+    }
+
+  });
+});
+
 app.listen(3280);
