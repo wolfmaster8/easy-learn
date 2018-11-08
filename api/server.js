925ad839f9488f1a9fb99ea953c18ba767571e42
wolfmaster8 <jflobom@gmail.com>
2018-11-08 17:23:46 -0200

    Added DB to AWS 🔀

--- api/server.js
+++ api/server.js
@@ -4,9 +4,9 @@ const mysql = require('mysql');
 const app = express();
 
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'easylearn.cfvh1qszjgl1.us-east-2.rds.amazonaws.com',
+  user: 'easylearnwolf8',
+  password: 'qFqp7wAAkB02',
   // password: 'root',
   port: 3306,
   // port: 8889,
@@ -22,7 +22,7 @@ conn.connect(function(e){
 });
 
 app.get('/create/usuarios', function(req, res){
- const sql = "CREATE TABLE usuarios (id_usuario INT(99) NOT NULL PRIMARY KEY auto_increment, nombre VARCHAR(50), apellido VARCHAR(50), email VARCHAR(50), pwd VARCHAR(255), rol INT(99), fecha_creacion TIMESTAMP, ultima_fecha_sesion DATE)";
+ const sql = "CREATE TABLE usuarios (id_usuario INT(99) NOT NULL PRIMARY KEY auto_increment, nombre VARCHAR(50), apellido VARCHAR(50), email VARCHAR(50), pwd VARCHAR(255), rol INT(99), fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , ultima_fecha_sesion DATE)";
  conn.query(sql, (err, result)=>{
    if(err) res.send(err);
    res.send(result)
@@ -30,7 +30,7 @@ app.get('/create/usuarios', function(req, res){
 });
 
 app.get('/create/cursos', function(req, res){
-  const sql = "CREATE TABLE cursos (id_curso INT(99) NOT NULL PRIMARY KEY auto_increment, titulo VARCHAR(99), descripcion TEXT, id_profesor INT(99), fecha_creacion TIMESTAMP)";
+  const sql = "CREATE TABLE cursos (id_curso INT(99) NOT NULL PRIMARY KEY auto_increment, titulo VARCHAR(99), descripcion TEXT, id_profesor INT(99), fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)";
   conn.query(sql, (err, result)=>{
     if(err) res.send(err);
     res.send(result)
@@ -38,7 +38,7 @@ app.get('/create/cursos', function(req, res){
 });
 
 app.get('/create/inscritos', function(req, res){
- const sql = "CREATE TABLE cursos_inscritos_assoc (id INT(99) NOT NULL PRIMARY KEY auto_increment, id_usuario INT(99), id_curso INT(99), fecha_creacion TIMESTAMP, FOREIGN KEY(id_curso) REFERENCES cursos(id_curso) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE ON UPDATE NO ACTION )";
+ const sql = "CREATE TABLE cursos_inscritos_assoc (id INT(99) NOT NULL PRIMARY KEY auto_increment, id_usuario INT(99), id_curso INT(99), fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , FOREIGN KEY(id_curso) REFERENCES cursos(id_curso) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE ON UPDATE NO ACTION )";
  conn.query(sql, (err, result)=>{
    if(err) res.send(err);
    res.send(result)
@@ -63,11 +63,18 @@ app.get('/create/avi', function(req, res){
 });
 
 app.get('/create/avi_ins', function(req, res){
-  const sql = "INSERT INTO cursos_inscritos_assoc (id_usuario, id_curso) VALUES (2, 1)";
+  const sql = "INSERT INTO cursos_inscritos_assoc (id_usuario, id_curso) VALUES (1, 1)";
   conn.query(sql, (err, result)=>{
     if(err) res.send(err);
     res.send(result)
   })
 });
 
+app.get('/show/:id', function(req, res){
+  const sql = "SELECT * FROM usuarios WHERE id_usuario=?";
+  conn.query(sql,[req.params.id], (err, rows, fields)=>{
+    if(err) res.send(err);
+    res.send(rows)
+  })
+});
 app.listen(3280);
